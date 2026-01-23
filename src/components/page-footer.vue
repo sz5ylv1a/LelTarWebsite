c65121cb81512730e451f4a3d06706d15ef2d34f
sz5ylv1a <sylvia.szalai.uwu@pm.me>
2026-01-23 11:49:14 +0100

    permanently removed .old folder and a few more things

--- src/components/page-footer.vue
+++ src/components/page-footer.vue
@@ -9,11 +9,11 @@ defineProps({
 <template>
 	<footer class="bg-body-tertiary px-3 py-3 mt-auto border-top border-body">
 		<div class="container-xxl">
-			<div class="row">
-				<div class="col text-start">
-					<p><span class="copyleft">&copy;</span> {{ entityName }} {{ fYear }}-{{ lYear }}. Game licensed under <a href="">MPL-2.0</a>, website <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a></p>
+			<div class="d-flex">
+				<div class="flex-fill text-start">
+					<p class="mb-0"><span class="copyleft">&copy;</span> {{ entityName }} {{ fYear }}-{{ lYear }}. Game licensed under <a href="">MPL-2.0</a>, website <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a></p>
 				</div>
-				<div class="col text-end d-inline-flex justify-content-end gap-3">
+				<div class="text-end d-inline-flex justify-content-end gap-3 ms-3">
 					<a class="nav-link" href="mailto:omgkiddoesta@pm.me" title="Contact us via e-mail!"><i class="bi bi-envelope-fill"></i></a>
 					<a class="nav-link" href="https://github.com/LohinSys" title="GitHub" target="_blank"><i class="bi bi-github"></i></a>
 				</div>
@@ -25,8 +25,7 @@ defineProps({
 <style scoped>
 footer {
 	box-shadow: 5px 0 10px #000;
-	height: 60px;
-	max-height: 60px;
+	min-height: 60px;
 }
 .copyleft {
 	display: inline-block;
