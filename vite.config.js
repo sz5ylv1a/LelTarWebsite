029c821ce989328bc5f45352b63a1b7b485d686a
sz5ylv1a <sylvia.szalai.uwu@pm.me>
2026-02-13 12:09:04 +0100

    update npm packages

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,7 @@ const baseRoute = 'LelTarWebsite'
 // https://vite.dev/config/
 export default defineConfig({
 	base: `/${baseRoute}/`,
-	publicPath: process.env.NODE_ENV === 'production'
-		? `/${baseRoute}/`
-		: '/',
+	publicPath: process.env.NODE_ENV === 'production' ? `/${baseRoute}/` : '/',
 	plugins: [vue()],
 	resolve: {
 		alias: {
