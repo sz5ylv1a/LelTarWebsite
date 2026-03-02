432652c0c1af62ed1560e398ce704e171e92ec45
sz5ylv1a <sylvia.szalai.uwu@pm.me>
2026-03-02 13:10:54 +0100

    sorta add an auth to this shit

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -1,27 +1,101 @@
-import ax from 'axios';
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import ax from "axios";
+import r from "./router.js";
 
 const api = ax.create({
 	baseURL: import.meta.env.VITE_API_URL,
-	timeout: 30000
-})
+	timeout: 30000,
+});
 
-api.interceptors.request.use(config => {
-	const token = localStorage.getItem('user_token')
-	if (token) { config.headers.Authorization = `Bearer ${token}` }
+api.interceptors.request.use((config) => {
+	const token = localStorage.getItem("user_token");
+	if (token) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
 
-	return config
-})
+	return config;
+});
 
 api.interceptors.use(
-	res => res,
-	error => {
+	(res) => res,
+	(error) => {
 		if (error.response?.status === 401) {
-			localStorage.removeItem('user_token')
-			localStorage.removeItem('user_name')
-			localStorage.removeItem('user_pass')
+			localStorage.removeItem("user_token");
+			localStorage.removeItem("user_data");
 		}
-		return Promise.reject(err)
-	}
-)
+		return Promise.reject(err);
+	},
+);
 
-// ------------------------------------------------------------ //
+// ------------------------------------------------ //
+
+export const useAuthStore = defineStore("auth", () => {
+	const token = ref(localStorage.getItem("user_token") || null);
+	const user = ref(JSON.parse(localStorage.getItem("user_data") || "null"));
+
+	const isLoggedIn = computed(() => !!token.value);
+
+	const assignedRole = ref(0);
+	switch (user.value?.role) {
+		case "Admin":
+			assignedRole.value = 1;
+			return;
+		case "Moderator":
+			assignedRole.value = 2;
+			return;
+		case "User":
+			assignedRole.value = 3;
+			return;
+		case "Banned":
+			assignedRole.value = 4;
+			return;
+	}
+
+	async function login(username, password) {
+		const { data } = await api.post("auth/login", {
+			username,
+			password,
+		});
+		token.value = data.token;
+		user.value = {
+			username: data.username,
+			role: data.role,
+		};
+
+		localStorage.setItem("user_token", data.token);
+		localStorage.setItem("user_data", JSON.stringify(user.value));
+
+		r.push("/");
+	}
+
+	async function register(username, email, password) {
+		const { data } = await api.post("auth/register", {
+			username,
+			email,
+			password,
+		});
+		token.value = data.token;
+		user.value = {
+			username: data.username,
+			role: data.role,
+		};
+
+		localStorage.setItem("user_token", data.token);
+		localStorage.setItem("user_data", JSON.stringify(user.value));
+
+		r.push("/");
+	}
+
+	async function logout() {
+		token.value = null;
+		user.value = null;
+
+		localStorage.removeItem("user_token");
+		localStorage.removeItem("user_data");
+
+		r.push("/");
+	}
+
+	return { token, user, isLoggedIn, assignedRole, login, register, logout };
+});
---
+++ src/scripts/router.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Home from './components/pages/Home.vue';
+import About from './components/pages/About.vue';
+import Changelog from './components/pages/Changelog.vue';
+import Downloads from './components/pages/Downloads.vue';
+import Leaderboards from './components/pages/Leaderboards.vue';
+
+import Login from './components/pages/Login.vue';
+import Register from './components/pages/Register.vue';
+
+import Profile from './components/pages/personal/Profile.vue';
+import Stats from './components/pages/personal/Stats.vue';
+import Settings from './components/pages/personal/Settings.vue';
+
+const base = `/${import.meta.env.VITE_BASE_ROUTE}/`;
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{ path: `${base}`, name: 'Home', component: Home },
+		{ path: `${base}about`, name: 'About', component: About },
+		{ path: `${base}changelog`, name: 'Changelog', component: Changelog },
+		{ path: `${base}downloads`, name: 'Downloads', component: Downloads },
+		{ path: `${base}leaderboards`, name: 'Leaderboards', component: Leaderboards },
+
+		{ path: `${base}login`, name: 'Login', component: Login },
+		{ path: `${base}register`, name: 'Register', component: Register },
+
+		{ path: `${base}profile`, name: 'Profile', component: Profile },
+		{ path: `${base}stats`, name: 'Stats', component: Stats },
+		{ path: `${base}settings`, name: 'Settings', component: Settings },
+
+		// a little fix for refreshes and going to non-root links manually returning bullshit 404 errors
+		{ path: `${base}:pathMatch(.*)*`, redirect: `${base}` }
+	]
+});
+
+router.beforeEach((to) => {
+	const token = localStorage.getItem('user_token');
+	const user = JSON.parse(localStorage.getItem('user_data') || 'null')
+
+	if (to.meta.guest && token) return { path: '/' }
+	if (to.meta.adminOnly && user?.role !== 'Admin') return { path: '/' }
+})
+
+export default router
