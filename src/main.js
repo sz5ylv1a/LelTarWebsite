5cdab559fc561f6d3fbfba4a4a7f7e60049d17ae
sz5ylv1a <sylvia.szalai.uwu@protonmail.com>
2026-03-02 14:44:47 +0100

    router don't work, oops!

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import './style.min.css';
 import App from './App.vue';
-import router from './scripts/router.js';
+import { router } from './scripts/router.js';
 
 createApp(App).use(createPinia()).use(router).mount('#app');
